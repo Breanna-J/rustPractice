005d0ebc19748a31a726a2c5574b1970a94b23bb
Breanna-J <kalebandbre@gmail.com>
2026-01-11 15:27:26 -0600

    apply a random number generator with approptiate outputs

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,10 +1,28 @@
 use std::io;
+use std:cmp::Ordering //standard comparison ordering
+use rand::Rng; //random number generator
 
 fn main() {
-    println!("Guess a number!");//println! means that println is a macro, rather than a function, 
+    
+    //println! means that println is a macro, rather than a function, 
     // and is used to print to print to the standard output
+    println!("Guess a number!");
+
+    // to use random number generators, you need to update dependencies in the toml file
+    // dependencies tell cargo the crates that your project depends. This one needs rand. add it to the top 
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    //{} is a placeholder that calls the variable inside 
+    println!("The secret number is: {secret_number}");
 
     println!("Please input your guess:");
+
+    //compare guess to the random number
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("Too low!"),
+        Ordering::Greater => println("Too High!"),
+        Ordering::Equal => println!("You Win!")
+    }
 
     //by default, rust variables are not mutable (you cant change them when they are initalized)
     // the mut keyword allows the variable value to change
@@ -14,11 +32,10 @@ fn main() {
         .read_line(&mut guess) //read line and replace the value of guess -- &mut guess references the variable
         .expect("Failed to read line."); //if the answer is not a string, as expected, print this. with no expect, you will get a warning for incorect input
 
-    println!("You guessed: {guess}"); //{} is a placeholder that calls the variable inside 
-
-    // to use random number generators, you need to update dependencies in the toml file
-    // dependencies tell cargo the crates that your project depends. This one needs rand.
+    println!("You guessed: {guess}"); 
 
     
+
+
     
 }
